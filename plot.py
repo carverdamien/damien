02ca0d452879b5e7108eb742b7151621a8ea4746
Damien Carver <carverdamien@gmail.com>
2016-09-27 18:04:46 +0200

    2 plots

--- plot.py
+++ plot.py
@@ -3,13 +3,25 @@ import pymongo, itertools, datetime
 import plotly.offline # import download_plotlyjs, init_notebook_mode, iplot, plot
 import plotly.graph_objs # import Scatter, Layout, Figure
 
+convert_read_to_datetime = lambda x : datetime.datetime.strptime(x[:-4], "%Y-%m-%dT%H:%M:%S.%f")
+convert_timestamp_to_datetime = lambda x : datetime.datetime.fromtimestamp(float(x))
+do_not_convert = lambda y : y
+
 curves = [{
     'label' : 'tunesysbench/23e73b86bc2a/mem/cache',
     'dbname' : 'tunesysbench',
     'collection' : 'dockerstats',
     'match' : {'Id' : {'$regex':'^23e73b86bc2a'}},
     'project' : {'x' : '$read', 'y': "$memory_stats.stats.cache"},
-    'convert' : {'x' : lambda x : datetime.datetime.strptime(x[:-4], "%Y-%m-%dT%H:%M:%S.%f"), 'y' : lambda y : y}
+    'convert' : {'x' : convert_read_to_datetime, 'y' : do_not_convert}
+},
+{
+    'label' : 'tunesysbench/23e73b86bc2a/trps',
+    'dbname' : 'tunesysbench',
+    'collection' : 'sysbench',
+    'match' : {'Id' : {'$regex':'^23e73b86bc2a'}},
+    'project' : {'x' : '$timestamp', 'y': "$trps"},
+    'convert' : {'x' : convert_timestamp_to_datetime, 'y' : do_not_convert}
 }]
 
 def XY(collection, dbname, match, project, convert, **kwargs):
