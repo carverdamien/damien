d2902b9b890cf6b606be65e9fcee3a39e3b2de2f
Damien Carver <carverdamien@gmail.com>
2016-09-27 15:19:44 +0200

    series/tunesysbench/prod.sh

---
+++ series/tunesysbench/prod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e -x
+DBNAME=tunesysbench
+damien() { ./damien --dbname ${DBNAME} $@; }
+SOURCEID=$(damien source add ./series/${DBNAME}/source.py)
+cat_config() {
+cat <<EOF
+{ 
+    "sourceId" : "${SOURCEID}",
+    "duration" : 1800,
+    "dbsize" : ${dbsize},
+    "mem_limit" : ${mem_limit},
+    "cpuset_cpus" : "0,1,4,5",
+    "threads" : 4
+}
+EOF
+}
+MAX=$((3*2**30))
+for mem_limit in $MAX $((5*MAX/6)) $((2*MAX/3))
+do
+    for dbsize in $((5*10**3)) $((10**4)) $((5*10**4)) $((10**5)) $((5*10**5)) $((10**6)) $((5*10**6)) $((10**7))
+    do
+	echo ${mem_limit} ${dbsize}
+	damien run new $(damien config add <(cat_config))
+    done
+done
+damien daemon || true
