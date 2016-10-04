ef8a2666f1a6291be5b77a24acd75e276703a294
Damien Carver <carverdamien@gmail.com>
2016-10-04 09:46:02 +0200

    dummy global view

--- series/tunesysbench/httpd.py
+++ series/tunesysbench/httpd.py
@@ -1,3 +1,60 @@
+@bottle.route('/tunesysbench/global/view.csv')
+def httpd_tunesysbench():
+    # x = dbsize
+    # y = metric (perf, ressource)
+    # label = [readonly, memlimit, aggregator, metric]
+    alldbsize = [e['_id']['dbsize']
+                 for e in db.config.aggregate([{
+                         '$group': {
+                             '_id': {
+                                 'dbsize':'$dbsize'
+                             }
+                         }
+                 }])]
+    allmem_limit = [e['_id']['mem_limit']
+                 for e in db.config.aggregate([{
+                         '$group': {
+                             '_id': {
+                                 'mem_limit':'$mem_limit'
+                             }
+                         }
+                 }])]
+    def rtps(run):
+        return [0,1] , [10,100]
+    allmetric = [rtps]
+    def max(T,M):
+        return np.amax(M)
+    def mean(T,M):
+        return np.mean(M)
+    allaggregator = [mean,max]
+    directory = os.path.join(cache_dir, 'tunesysbench/global')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    filename = os.path.join(directory, 'view.csv')
+    with open(filename, 'w') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(['x','y','label'])
+        for dbsize in alldbsize:
+            x = dbsize
+            for mem_limit in allmem_limit:
+                for readonly, match_config in [('r',{'oltp_read_only':True, 'mem_limit' : mem_limit}), ('rw',{'mem_limit':mem_limit})]:
+                    match_config['dbsize'] = dbsize
+                    for config in db.config.find(match_config):
+                        configId = config['configId']
+                        for run in db.run.find({'configId':configId}):
+                            for metric in allmetric:
+                                T,M = metric(run)
+                                for aggregate in allaggregator:
+                                    y = aggregate(T,M)
+                                    label = '.'.join([readonly,
+                                                      ''.join([aggregate.func_name,
+                                                               '(',
+                                                               metric.func_name,
+                                                               ')']),])
+                                    csvwriter.writerow([x,y,label])
+    with open(filename) as f:
+        return f.read()
+    return "oops"
 @bottle.route('/sysbench')
 def httpd_sysbench_listId():
     table = []
