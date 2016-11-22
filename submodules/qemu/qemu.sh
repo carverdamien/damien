84bed40bda85c7e52bef063ed2f45f01c3d994ce
Damien Carver <carverdamien@gmail.com>
2016-11-22 16:48:07 +0100

    qemu.sh

---
+++ submodules/qemu/qemu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo qemu-system-x86_64 \
+-cpu host -smp cores=4,threads=2,sockets=1 \
+-m 4G \
+-enable-kvm \
+-kernel /boot/vmlinuz-4.6.0+ \
+-chardev stdio,id=stdio,mux=on,signal=off \
+-device virtio-serial-pci \
+-device virtconsole,chardev=stdio \
+-mon chardev=stdio \
+-display none \
+-net user \
+-net nic,model=virtio \
+-redir tcp:80::80 \
+-redir tcp:2222::22 \
+-drive file=image,if=virtio,cache=none \
+-append 'root=/dev/vda console=hvc0'
