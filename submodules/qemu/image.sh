b9c8294dae0eafa0d911a353d854e26cf6a9e064
Damien Carver <carverdamien@gmail.com>
2016-11-23 13:50:00 +0100

    chown -R root:root

--- submodules/qemu/image.sh
+++ submodules/qemu/image.sh
@@ -2,7 +2,7 @@
 set -e -x
 IMAGE=image
 : ${SIZE:=$((2*2**30))}
-! (mount | grep ${IMAGE}.tmp)
+! mount | grep ${IMAGE}.tmp
 dd if=/dev/zero of=${IMAGE}.tmp bs=1M count=$((SIZE / 2**20))
 mkfs.ext4 -F ${IMAGE}.tmp
 tune2fs -O ^has_journal ${IMAGE}.tmp
@@ -10,6 +10,7 @@ mkdir .mnt
 sudo mount ${IMAGE}.tmp .mnt
 sudo cp -a chroot/. .mnt/.
 sudo cp -a root .mnt/.
+sudo chown -R root:root .mnt/root
 sudo umount .mnt
 rmdir .mnt
 mv ${IMAGE}.tmp ${IMAGE}
--- submodules/qemu/qemu.sh
+++ submodules/qemu/qemu.sh
@@ -3,7 +3,7 @@ sudo qemu-system-x86_64 \
 -cpu host -smp cores=4,threads=2,sockets=1 \
 -m 4G \
 -enable-kvm \
--kernel /boot/vmlinuz-4.6.0+ \
+-kernel /home/dc/Git/linux/build/develop/arch/x86/boot/bzImage \
 -chardev stdio,id=stdio,mux=on,signal=off \
 -device virtio-serial-pci \
 -device virtconsole,chardev=stdio \
