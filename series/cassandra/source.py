5bb71d612de286850dff491825d89b3c418fee7b
Damien Carver <carverdamien@gmail.com>
2016-12-08 15:17:41 +0100

    cassandra: init writes.

--- series/cassandra/source.py
+++ series/cassandra/source.py
@@ -123,7 +123,7 @@ class Boot(threading.Thread):
 cassandra_expected_output = """total,{:s}{total_ops},{:s}{op_s},{:s}{pk_s},{:s}{row_ps},{:s}{mean},{:s}{med},{:s}{perc95},{:s}{perc99},{:s}{perc999},{:s}{max},{:s}{time},{:s}{stderr},{:s}{erros},{:s}{gc_count},{:s}{max_ms},{:s}{sum_ms},{:s}{sdv_ms},{:s}{mb}"""
 cassandra_parser = parse.compile(cassandra_expected_output)
 class Cassandra(threading.Thread):
-    def __init__(self, container, schedule, threads):
+    def __init__(self, container, schedule, writes):
         super(Cassandra, self).__init__()
         container = docker.Client().inspect_container(container)
         while not container['State']['Running']:
@@ -132,21 +132,26 @@ class Cassandra(threading.Thread):
             container = docker.Client().inspect_container(container)
         self.container = container
         self.schedule = schedule
-        self.threads = threads
+        time.sleep(60)
+        cmd = ['write', 'n=%s' % writes]
+        self.cassandra(cmd)
+    def cassandra(self, cmd):
+        cmd = ['cassandra-stress'] + cmd
+        dockerexec = docker.Client().exec_create(container=self.container, cmd=cmd)
+        for line in docker.Client().exec_start(dockerexec, stream=True):
+            timestamp = time.time()
+            res = cassandra_parser.search(line)
+            if res != None:
+                res.named['Id'] = self.container['Id']
+                res.named['timestamp'] = timestamp
+                db.cassandra.insert_one(res.named)
+            elif line not in ['','\n']:
+                print(line[:-1])
     def run(self):
         for delay, duration in self.schedule:
             time.sleep(delay)
-            cmd = ['cassandra-stress', 'write', 'duration=%s' % duration, '-rate', 'threads=%s' % self.threads]
-            dockerexec = docker.Client().exec_create(container=self.container, cmd=cmd)
-            for line in docker.Client().exec_start(dockerexec, stream=True):
-                timestamp = time.time()
-                res = cassandra_parser.search(line)
-                if res != None:
-                    res.named['Id'] = self.container['Id']
-                    res.named['timestamp'] = timestamp
-                    db.cassandra.insert_one(res.named)
-                elif line not in ['','\n']:
-                    print(line[:-1])
+            cmd = ['read', 'duration=%s' % duration]
+            self.cassandra(cmd)
 
 sysbench_bin_path = './sysbench/sysbench'
 sysbench_lua_path = './sysbench/tests/db/oltp.lua'
