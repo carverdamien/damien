fd5134f706fcee9aa63ba948ea4f6b0eab357ba9
Damien Carver <carverdamien@gmail.com>
2017-04-12 17:41:55 +0200

    some.sh

---
+++ submodules/twitter_memtier/memcached_client/rps2.sh
@@ -0,0 +1,1 @@
+./memcached_client/loader -a ./twitter_dataset/twitter_dataset_2x -s memcached_client/docker_servers.txt -g 0.8 -T 1 -c 200 -w 8
---
+++ submodules/twitter_memtier/memcached_client/scale2.sh
@@ -0,0 +1,1 @@
+./memcached_client/loader -a ./twitter_dataset/twitter_dataset_unscaled -o ./twitter_dataset/twitter_dataset_2x -s memcached_client/docker_servers.txt -w 4 -S 2 -D 4096 -j -T 1
