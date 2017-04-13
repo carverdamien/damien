e0e9709d373b761e1fede53ad0f6b2f2bfe6a878
Damien Carver <carverdamien@gmail.com>
2017-04-13 17:34:37 +0200

    docker-compose

--- submodules/cloudsuite/datacaching/docker-compose-scale-up.sh
+++ submodules/cloudsuite/datacaching/docker-compose-scale-up.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -e -x
+: RPS ${RPS:=}
 : SERVER ${SERVER:=2}
-echo "SERVERS=$(eval echo server_{1..${SERVER}})" > .env
+: WORKER ${WORKER:=${SERVER}}
+: SCALE ${SCALE:=2}
+: MEMORY ${MEMORY:=$((300*SCALE))}
+: CONNECTION ${CONNECTION:=200}
+: MASTER ${MASTER:=}
+env() {
+cat <<EOF
+RPS=${RPS}
+SERVERS=$(eval echo server_{1..${SERVER}})
+WORKER=${WORKER}
+SCALE=${SCALE}
+MEMORY=${MEMORY}
+CONNECTION=${CONNECTION}
+MASTER=${MASTER}
+EOF
+}
+env | tee .env 
 docker-compose scale server=${SERVER}
 docker-compose up -d
