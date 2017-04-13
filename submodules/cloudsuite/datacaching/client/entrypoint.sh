9f560bba260eedad1a7f1cbc40f6577ce0dd7f21
Damien Carver <carverdamien@gmail.com>
2017-04-13 13:40:22 +0200

    docker-compose-scale-up.sh

--- submodules/cloudsuite/datacaching/client/entrypoint.sh
+++ submodules/cloudsuite/datacaching/client/entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e -x
 
+servers() { for s in ${SERVERS}; do echo "${s}, 11211"; done; }
+scale()  { ./loader -a ${UNSCALED_DATA} -o ${DATA} -s <(servers) -w ${WORKER} -S ${SCALE} -D ${MEMORY} -j -T 1; }
+maxrps() { ./loader -a ${DATA} -s <(servers) -g ${GET_SET_RATIO} -T 1 -c ${CONNECTION} -w ${WORKER}; }
+fixrps() { ./loader -a ${DATA} <(servers) -g ${GET_SET_RATIO} -T 1 -c ${CONNECTION} -w ${WORKER} -e -r ${RPS}; }
+
 # Defaults
 : ${UNSCALED_MEMORY:=300}
 : ${UNSCALED_DATA:=/data/twitter_dataset_unscaled}
 : ${SCALE:=2}
 : ${MEMORY:=$((UNSCALED_MEMORY * SCALE))}
 : ${DATA:="${UNSCALED_DATA}_${SCALE}x"}
-: ${WORKER:=1}
-: ${CONNECTION:=1}
 : ${GET_SET_RATIO:=0.8}
+: ${SERVERS:=server}
+: ${WORKER:=$(wc -l <(servers))}
+: ${CONNECTION:=${WORKER}}
 
 main() {
     [ -f "${UNSCALED_DATA}" ]
@@ -20,10 +26,5 @@ main() {
 	*) fixrps;;
     esac
 }
-
-servers() { echo "server, 11211"; }
-scale()  { ./loader -a ${UNSCALED_DATA} -o ${DATA} -s <(servers) -w ${WORKER} -S ${SCALE} -D ${MEMORY} -j -T 1; }
-maxrps() { ./loader -a ${DATA} -s <(servers) -g ${GET_SET_RATIO} -T 1 -c ${CONNECTION} -w ${WORKER}; }
-fixrps() { ./loader -a ${DATA} <(servers) -g ${GET_SET_RATIO} -T 1 -c ${CONNECTION} -w ${WORKER} -e -r ${RPS}; }
 
 main
---
+++ submodules/cloudsuite/datacaching/docker-compose-scale-up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SERVER=2
+echo "SERVERS=$(eval echo server_{1..${SERVER}})" > .env
+docker-compose scale server=${SERVER}
+docker-compose up -d
