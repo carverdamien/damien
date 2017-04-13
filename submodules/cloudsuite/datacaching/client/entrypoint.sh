285b8a223ed5524da68310b8d21dfe811d781081
Damien Carver <carverdamien@gmail.com>
2017-04-13 13:47:20 +0200

    prints

--- submodules/cloudsuite/datacaching/client/entrypoint.sh
+++ submodules/cloudsuite/datacaching/client/entrypoint.sh
@@ -7,15 +7,15 @@ maxrps() { ./loader -a ${DATA} -s <(servers) -g ${GET_SET_RATIO} -T 1 -c ${CONNE
 fixrps() { ./loader -a ${DATA} <(servers) -g ${GET_SET_RATIO} -T 1 -c ${CONNECTION} -w ${WORKER} -e -r ${RPS}; }
 
 # Defaults
-: ${UNSCALED_MEMORY:=300}
-: ${UNSCALED_DATA:=/data/twitter_dataset_unscaled}
-: ${SCALE:=2}
-: ${MEMORY:=$((UNSCALED_MEMORY * SCALE))}
-: ${DATA:="${UNSCALED_DATA}_${SCALE}x"}
-: ${GET_SET_RATIO:=0.8}
-: ${SERVERS:=server}
-: ${WORKER:=$(wc -l <(servers))}
-: ${CONNECTION:=${WORKER}}
+: UNSCALED_MEMORY ${UNSCALED_MEMORY:=300}
+: UNSCALED_DATA ${UNSCALED_DATA:=/data/twitter_dataset_unscaled}
+: SCALE ${SCALE:=2}
+: MEMORY ${MEMORY:=$((UNSCALED_MEMORY * SCALE))}
+: DATA ${DATA:="${UNSCALED_DATA}_${SCALE}x"}
+: GET_SET_RATIO ${GET_SET_RATIO:=0.8}
+: SERVERS ${SERVERS:=server}
+: WORKER ${WORKER:=$(wc -l <(servers))}
+: CONNECTION ${CONNECTION:=${WORKER}}
 
 main() {
     [ -f "${UNSCALED_DATA}" ]
--- submodules/cloudsuite/datacaching/docker-compose-scale-up.sh
+++ submodules/cloudsuite/datacaching/docker-compose-scale-up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-SERVER=2
+set -e -x
+: SERVER ${SERVER:=2}
 echo "SERVERS=$(eval echo server_{1..${SERVER}})" > .env
 docker-compose scale server=${SERVER}
 docker-compose up -d
