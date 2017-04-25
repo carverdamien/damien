8c7d36eca260777101ffcd728173b12a663d2f0a
Damien Carver <carverdamien@gmail.com>
2017-04-25 10:31:56 +0200

    json output

--- submodules/cloudsuite/datacaching/client/stats.c
+++ submodules/cloudsuite/datacaching/client/stats.c
@@ -100,13 +100,15 @@ void printGlobalStats(struct config* config) {
   pthread_mutex_lock(&stats_lock);
   struct timeval currentTime;
   gettimeofday(&currentTime, NULL);
-  double timeDiff = currentTime.tv_sec - global_stats.last_time.tv_sec + 1e-6*(currentTime.tv_sec - global_stats.last_time.tv_sec);
+  double t = currentTime.tv_sec + 1e-6*currentTime.tv_usec;
+  double timeDiff = currentTime.tv_sec - global_stats.last_time.tv_sec + 1e-6*(currentTime.tv_usec - global_stats.last_time.tv_usec);
   double rps = global_stats.requests/timeDiff;
   double std = getStdDev(&global_stats.response_time);
   double q90 = findQuantile(&global_stats.response_time, .90);
   double q95 = findQuantile(&global_stats.response_time, .95);
   double q99 = findQuantile(&global_stats.response_time, .99);
 
+  /*
   printf("%10s,%8s,%16s, %8s,%11s,%10s,%13s,%10s,%10s,%10s,%12s,%10s,%10s,%11s,%14s\n", "timeDiff", "rps", "requests", "gets", "sets",  "hits", "misses", "avg_lat", "90th", "95th", "99th", "std", "min", "max", "avgGetSize");
   printf("%10f, %9.1f,  %10d, %10d, %10d, %10d, %10d, %10f, %10f, %10f, %10f, %10f, %10f, %10f, %10f\n", 
 		timeDiff, rps, global_stats.requests, global_stats.gets, global_stats.sets, global_stats.hits, global_stats.misses,
@@ -115,8 +117,49 @@ void printGlobalStats(struct config* config) {
   printf("Outstanding requests per worker:\n");
   for(i=0; i<config->n_workers; i++){
     printf("%d ", config->workers[i]->n_requests);
-  } 
-  printf("\n");
+    }
+  printf("\n");*/
+  printf("{ ");
+  int i;
+  for(i=0; i<config->n_workers; i++) {
+    printf("worker%d:%d, ", i, config->workers[i]->n_requests);
+  }
+    printf(
+	 "time:%f, "
+	 //"timeDiff:%f, "
+	 "rps:%f, "
+	 "requests:%d, "
+	 "gets:%d, "
+	 "sets:%d, "
+	 "hits:%d, "
+	 "misses:%d, "
+	 "avg_lat:%f, "
+	 "90th:%f, "
+	 "95th:%f, "
+	 "99th:%f, "
+	 "std:%f, "
+	 "min:%f, "
+	 "max:%f, "
+	 "avgGetSize:%f }\n"
+	 ,
+	 t,
+	 //timeDiff,
+	 rps,
+	 global_stats.requests,
+	 global_stats.gets,
+	 global_stats.sets,
+	 global_stats.hits,
+	 global_stats.misses,
+	 1000*getAvg(&global_stats.response_time),
+	 1000*q90,
+	 1000*q95,
+	 1000*q99,
+	 1000*std,
+	 1000*global_stats.response_time.min,
+	 1000*global_stats.response_time.max,
+	 getAvg(&global_stats.get_size)
+	 );
+
   //Reset stats
   memset(&global_stats, 0, sizeof(struct memcached_stats));
   global_stats.response_time.min = 1000000;
